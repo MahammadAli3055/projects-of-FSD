6b748ac0a8a734e58e5be3a64ee751da8b7536eb
MahammadAli3055 <palagirimahammadali@gmail.com>
2025-09-25 13:53:09 +0530

    Add files via upload

---
+++ function Counter.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react';
+function Counter(){
+const[count,setCount]=useState(0);
+const handleIncrement=()=>{
+setCount(count+1);
+};
+return(
+<div style={{ textAlign:'center',maeginTop:'50px'}}>
+<h2>React Counter using useState</h2>
+<p style={{ fontSize:'24px'}}>Count:{count}</p>
+<button onClick={handleIncrement} style={{fontsize:'16px',padding:'10px 20px'}}>
+Increment
+</button>
+</div>
+);
+}
+export default Counter;
